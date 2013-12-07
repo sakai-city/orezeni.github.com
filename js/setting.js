deb858210dc89e520700ea1cc752b9ded41d7735
sakai-city <njip.fib@gmail.com>
2013-12-07 14:53:45 +0900

    chnge v2

--- js/setting.js
+++ js/setting.js
@@ -10,7 +10,7 @@ Taxes.taxRate = 0.06; // 住民税率
 
 var OpenSpending = OpenSpending || {};
 
-OpenSpending.identifier = 'sakai_budget';
+OpenSpending.identifier = 'sakai_budget_v2';
 OpenSpending.year = '2013';
 
 OpenSpending.Styles = OpenSpending.Styles || {};
