bc9563cd368f25c84bac77ac57587c385e21593a
sakai <njip.fib@gmail.com>
2014-04-27 15:14:32 +0900

    sakai_budjet

--- js/setting.js
+++ js/setting.js
@@ -10,8 +10,8 @@ Taxes.taxRate = 0.06; // 住民税率
 
 var OpenSpending = OpenSpending || {};
 
-OpenSpending.identifier = 'sakai_budget_v3';
-OpenSpending.year = '2013';
+OpenSpending.identifier = 'sakai_budget';
+OpenSpending.year = '2014';
 
 OpenSpending.Styles = OpenSpending.Styles || {};
 
